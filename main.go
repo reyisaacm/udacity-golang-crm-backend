0d1f941b9260581bce9ff03b5ee2c507505c1f22
Reynhart-web <lightning2006@gmail.com>
2023-11-13 09:54:58 +0000

    add seed and get list customer

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -19,10 +20,43 @@ type Customer struct {
 var dataStore = []Customer{}
 
 func getCustomerList(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusOK)
 
+	json.NewEncoder(w).Encode(dataStore)
 }
 
 func main() {
+	//Initial seed
+	var custList = []Customer{
+		{
+			ID: "1",
+			Name: "Customer 1",
+			Role: "Sales Engineer",
+			Email: "cust1@dummy.com",
+			Phone: "123456789",
+			Contacted: false,
+		},
+		{
+			ID: "2",
+			Name: "Customer 2",
+			Role: "Civil Engineer",
+			Email: "cust2@dummy.com",
+			Phone: "987654321",
+			Contacted: true,
+		},
+		{
+			ID: "3",
+			Name: "Customer 3",
+			Role: "Aeronautics Engineer",
+			Email: "cust3@dummy.com",
+			Phone: "567891234",
+			Contacted: true,
+		},
+	}
+
+	dataStore = append(dataStore, custList...)
+
 	// Instantiate a new router by invoking the "NewRouter" handler
 	router := mux.NewRouter()
 
